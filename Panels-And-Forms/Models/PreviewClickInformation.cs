9e16b770539ef7d60cb02a0457eefc54b4eaba17
Byron Hall <byronhall@Byrons-Mac-mini.local>
2024-03-03 09:02:07 -0600

    added class for preview clicks

---
+++ Panels-And-Forms/Models/PreviewClickInformation.cs
@@ -0,0 +1,8 @@
+namespace Panels_And_Forms.Models;
+
+public class PreviewClickInformation
+{
+    public string State { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string? City { get; set; }
+}
